da7230db4dedf160dc812a5fe5c400903321729a
geekbitcreations <dbarndt@hawk.iit.edu>
2020-12-01 20:52:48 -0600

    removed script

--- js/main.js
+++ js/main.js
@@ -1,28 +1,27 @@
-<script type="text/javascript">
-  var einput = document.getElementById("email").value;
-  var ninput = document.getElementById("name").value;
-    
-    function validateForm()
+var einput = document.getElementById("email").value;
+var ninput = document.getElementById("name").value;
+  
+  function validateForm()
+  {
+    // Check if the input for email and name is empty.
+    if (einput == "" || ninput == "") 
     {
-      // Check if the input for email and name is empty.
-      if (einput == "" || ninput == "") 
-      {
-        alert("Error: Please enter your name and email.");
-        document.getElementById("submitBtn").disabled = true;
-      }
-
-      else 
-      {
-        document.getElementById("submitBtn").disabled = false;
-      }
+      alert("Error: Please enter your name and email.");
+      document.getElementById("submitBtn").disabled = true;
     }
 
-    submitBtn.addEventListener("click", displayInfo())
-
-    function displayInfo() 
+    else 
     {
-        document.getElementById("einput").value = email;
-        document.getElementById("ninput").value = name;
-      alert(value);
+      document.getElementById("submitBtn").disabled = false;
     }
-</script>
+  }
+
+  submitBtn.addEventListener("click", displayInfo())
+
+  function displayInfo() 
+  {
+      document.getElementById("einput").value = email;
+      document.getElementById("ninput").value = name;
+    alert(value);
+  }
+  
