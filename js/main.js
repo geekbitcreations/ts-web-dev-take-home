4977171ae6ef45cb1860a88f9f939878499b4384
geekbitcreations <dbarndt@hawk.iit.edu>
2020-12-01 20:39:55 -0600

    working on javascript

---
+++ js/main.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+  var einput = document.getElementById("email").value;
+  var ninput = document.getElementById("name").value;
+    
+    function validateForm()
+    {
+      // Check if the input for email and name is empty.
+      if (einput == "" || ninput == "") 
+      {
+        alert("Error: Please enter your name and email.");
+        document.getElementById("submitBtn").disabled = true;
+      }
+
+      else 
+      {
+        document.getElementById("submitBtn").disabled = false;
+      }
+    }
+
+    submitBtn.addEventListener("click", displayInfo())
+
+    function displayInfo() 
+    {
+        document.getElementById("einput").value = email;
+        document.getElementById("ninput").value = name;
+      alert(value);
+    }
+</script>
